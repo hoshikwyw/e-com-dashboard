17e9f28f47351f17e106800178ac7afa25b80988
hoshikwyw <khaingwutyiwin1712@gmail.com>
2025-06-10 15:28:33 +0630

    Profile details card ui setUp

--- src/components/common/ProfileDetailCard.tsx
+++ src/components/common/ProfileDetailCard.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent } from "../ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
-const ProfileDetailCard = () => {
-  return (
-    <div>ProfileDetailCard</div>
-  )
+interface ProfileDetailCardProps {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+  avatarUrl?: string;
+  verified: boolean;
 }
 
-export default ProfileDetailCard
+const ProfileDetailCard: React.FC<ProfileDetailCardProps> = ({
+  id,
+  name,
+  email,
+  phone,
+  address,
+  avatarUrl,
+  verified,
+}) => {
+  return (
+    <>
+    <Card>
+        <CardContent>
+            <div>
+                <Avatar>
+                    <AvatarImage></AvatarImage>
+                    <AvatarFallback></AvatarFallback>
+                </Avatar>
+            </div>
+        </CardContent>
+    </Card>
+    </>
+  );
+};
+
+export default ProfileDetailCard;
